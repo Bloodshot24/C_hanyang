657531885c179f000ae54c89b829c5f09c0ecf09
Bastien Genova <bastien.genova@gmail.com>
2026-09-15 14:05:24 +0900

    Initial Commit

---
+++ homework1_Bastien_GENOVA_git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework1_Bastien_GENOVA_git
+//
+//  Created by Bastien Genova on 15/09/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
